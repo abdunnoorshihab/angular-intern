381c55dc41c80fad635986972d054ab29a01d17e
ikbalsazib <ikbal.sazib@gmail.com>
2024-05-07 14:20:26 +0600

    Layout Overview

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'layout',
+    loadChildren: () => import('./pages/layout/layout.module').then(m => m.LayoutModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
